17cc9b08528b91c2b8f4133bc03fccb3b4ca8f3e
NNrepos <nivimano@gmail.com>
2021-05-26 21:18:16 +0300

    fixed parse program

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,8 +1,12 @@
 #include "parser.tab.hpp"
+#include "parse_utils.h"
 
 char textbuff[1024];
 char* textbuffptr;
 
 int main(){
-    return yyparse();
+    yyparse();
+
+    // moved parse program here because syntax error happens after the reduce
+    ParseUtils::instance().ParseProgram(yylineno);
 }
